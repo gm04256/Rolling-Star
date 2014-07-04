670cbb6f75818adafab3ffece315f41eb471d22e
Ma Yan <896098846@qq.com>
2014-07-04 11:55:03 +0900

    Project Created

---
+++ Rolling Star/Rolling Star/BGMViewController.m
@@ -0,0 +1,51 @@
+//
+//  BGMViewController.m
+//  Rolling Star
+//
+//  Created by 馬 岩 on 14-7-4.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import "BGMViewController.h"
+#import "BGMMyScene.h"
+
+@implementation BGMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [BGMMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
---
+++ Rolling Star/Rolling Star/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rolling Star
+//
+//  Created by 馬 岩 on 14-7-4.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGMAppDelegate class]));
+	}
+}
